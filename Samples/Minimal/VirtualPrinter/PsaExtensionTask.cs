6096f0767d59492fb7c8aa01d91c84c0a6692b7c
clown <tt.clown@gmail.com>
2026-02-17 14:35:35 +0900

    Add minimal project for PSA v4 virtual printer

---
+++ Samples/Minimal/VirtualPrinter/PsaExtensionTask.cs
@@ -0,0 +1,75 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+namespace Cube.Psa.Minimal;
+
+using Windows.ApplicationModel.Background;
+using Windows.Graphics.Printing.PrintSupport;
+
+/* ------------------------------------------------------------------------- */
+///
+/// PsaExtensionTask
+///
+/// <summary>
+/// Minimal implementation of the Windows.PrintSupportExtension feature.
+/// </summary>
+///
+/// <remarks>
+/// This implementation is provided only to satisfy platform requirements.
+/// The application does not rely on this feature, but a definition is
+/// required to avoid runtime errors. Therefore, this task simply reports
+/// a resolved state.
+/// </remarks>
+///
+/* ------------------------------------------------------------------------- */
+public sealed class PsaExtensionTask : IBackgroundTask
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Run
+    ///
+    /// <summary>
+    /// Performs the work of a background task.
+    /// </summary>
+    ///
+    /// <param name="task">
+    /// An interface to an instance of the background task.
+    /// </param>
+    ///
+    /* --------------------------------------------------------------------- */
+    public void Run(IBackgroundTaskInstance task)
+    {
+        var deferral = task?.GetDeferral();
+        if (deferral is null) return;
+
+        task.Canceled += (_, _) => deferral.Complete();
+
+        var details = task.TriggerDetails as PrintSupportExtensionTriggerDetails;
+        if (details?.Session is null)
+        {
+            deferral.Complete();
+            return;
+        }
+
+        details.Session.PrintTicketValidationRequested += (_, e) =>
+        {
+            using (e.GetDeferral()) e.SetPrintTicketValidationStatus(WorkflowPrintTicketValidationStatus.Resolved);
+        };
+
+        details.Session.Start();
+    }
+}
---
+++ Samples/Minimal/VirtualPrinter/PsaVirtualPrinterTask.cs
@@ -0,0 +1,84 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+namespace Cube.Psa.Minimal;
+
+using System;
+using Windows.ApplicationModel.Background;
+using Windows.Graphics.Printing.Workflow;
+using Windows.Storage;
+
+/* ------------------------------------------------------------------------- */
+///
+/// PsaVirtualPrinterTask
+///
+/// <summary>
+/// Minimal implementation of the Windows.PrintSupportVirtualPrinterWorkflow
+/// feature for XPS-to-PDF conversion.
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+public sealed class PsaVirtualPrinterTask : IBackgroundTask
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Run
+    ///
+    /// <summary>
+    /// Performs the work of a background task.
+    /// </summary>
+    ///
+    /// <param name="task">
+    /// An interface to an instance of the background task.
+    /// </param>
+    ///
+    /* --------------------------------------------------------------------- */
+    public void Run(IBackgroundTaskInstance task)
+    {
+        var deferral = task?.GetDeferral();
+        if (deferral is null) return;
+
+        var details = task.TriggerDetails as PrintWorkflowVirtualPrinterTriggerDetails;
+        var session = details?.VirtualPrinterSession;
+        if (session is null) return;
+
+        session.VirtualPrinterDataAvailable += async (_, e) =>
+        {
+            var status = PrintWorkflowSubmittedStatus.Failed;
+
+            try
+            {
+                var src = e.SourceContent;
+                if (src.ContentType != "application/oxps") throw new NotSupportedException("Microsoft only provides conversion from XPS");
+
+                var file = await e.GetTargetFileAsync();
+                var stream = await file.OpenAsync(FileAccessMode.ReadWrite);
+                var converter = e.GetPdlConverter(PrintWorkflowPdlConversionType.XpsToPdf);
+                await converter.ConvertPdlAsync(e.GetJobPrintTicket(), src.GetInputStream(), stream.GetOutputStreamAt(0));
+
+                status = PrintWorkflowSubmittedStatus.Succeeded;
+            }
+            finally
+            {
+                e.CompleteJob(status);
+                deferral.Complete();
+            }
+        };
+
+        session.Start();
+    }
+}
